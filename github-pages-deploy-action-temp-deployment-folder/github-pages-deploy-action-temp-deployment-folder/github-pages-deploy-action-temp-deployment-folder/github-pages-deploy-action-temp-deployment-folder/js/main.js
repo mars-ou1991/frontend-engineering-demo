81f6feee40ea665529776667255aeed224467b74
mars-ou1991 <foolnofool@gmail.com>
2021-05-26 02:48:09 +0000

    Deploying to gh-pages from @ mars-ou1991/frontend-engineering-demo@c8316dbbbd3baffa684a4bbdb1484b0696a8b802 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/js/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import router from './router/index'
+Vue.use(VueRouter)
+
+new Vue({
+  router: router,
+  data() {
+    return {
+      transitionName: 'fold-left'
+    }
+  },
+  watch: {
+    '$route'(to, from) {
+      const toDepth = to.path.split('/').length
+      const fromDepth = from.path.split('/').length
+      this.transitionName = toDepth < fromDepth ? 'fold-right' : 'fold-left'
+    }
+  }
+}).$mount('#list-wrap')
